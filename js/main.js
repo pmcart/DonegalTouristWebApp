093a2295e4a99455e32432c5303af06ed65fbcc3
pmcart <patrickmcart@gmail.com>
2014-11-15 17:15:28 +0000

    no message

--- js/main.js
+++ js/main.js
@@ -1,57 +1,24 @@
-//jQuery(function() {
-//  var q = parseQueryString(window.location.search);
-//  if (q.url) {
-//    $('input[name="url"]').val(q.url);
-//
-//    $('.loading').show();
-//
-//    var display = function(info) {
-//      $('.loading').hide();
-//      $('.results').show();
-//
-//      rawData = info.raw;
-//      var summaryInfo = info.summary;
-//      var properties = rawData[info.dbpediaUrl];
-//
-//      for (key in summaryInfo) {
-//        $('.summary .' + key).text(summaryInfo[key]);
-//      }
-//      $('.summary .thumbnail').attr('src', summaryInfo.image);
-//      var dataAsJson = JSON.stringify(summaryInfo, null, '    ')
-//      $('.summary .raw').val(dataAsJson);
-//
-//      // Raw Data Summary
-//      var count = 0;
-//      for (key in properties) {
-//        count += 1;
-//        $('.data-summary .properties').append(key + '\n');
-//      }
-//      $('.data-summary .count').text(count);
-//
-//      // raw JSON
-//      var dataAsJson = JSON.stringify(rawData, null, '    ')
-//      $('.results-json').val(dataAsJson);
-//
-//      $('html,body').animate({
-//        scrollTop: $('#demo').offset().top
-//        },
-//        'slow'
-//      );
-//    };
-//
-//    WIKIPEDIA.getData(q.url, display, function(error) {
-//        alert(error);
-//      }
-//    );
-//  }
-//
-//  $('.js-data-summary').click(function(e) {
-//    $('.data-summary').show();
-//  });
-//});
 
-// TODO: search of wikipedia
-// http://en.wikipedia.org/w/api.php?action=query&format=json&callback=test&list=search&srsearch=%richard%
+function initialize() {
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(55.0237, -7.4275),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+
+
+function setMapLocation(lat,long)
+{
+    console.log("Lat is " + lat + " Long is " + long);
+    gMap = new google.maps.Map(document.getElementById('map_canvas')); 
+    gMap.setZoom(15);      // This will trigger a zoom_changed on the map
+    gMap.setCenter(new google.maps.LatLng(lat, long));
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 // Parse a URL query string (?xyz=abc...) into a dictionary.
 parseQueryString = function(q) {
@@ -129,8 +96,6 @@ angularApp.controller('mainController', ['$scope', '$compile',
     
     function($scope, $compile) {
         
-    //$scope.attractions =[];
-        
     $scope.selectedItemWiki = "This is random wiki text";
     
     $scope.attractions = [
@@ -141,22 +106,28 @@ angularApp.controller('mainController', ['$scope', '$compile',
      img3Src : "/NewWebProject/img/grianan/g3.jpg",
      img4Src : "/NewWebProject/img/grianan/g4.jpg",
      img5Src : "/NewWebProject/img/grianan/g5.jpg",
+     lat: 55.0237,
+     long: -7.4275,
     },
          {name: "Malin Head",
      wikipediaURL : 'http://en.wikipedia.org/wiki/Malin_Head',
-     img1Src : "/NewWebProject/img/malin/m1.Jpg",
+     img1Src : "/NewWebProject/img/malin/m1.jpg",
      img2Src : "/NewWebProject/img/malin/m2.jpg",
      img3Src : "/NewWebProject/img/malin/m3.jpg",
      img4Src : "/NewWebProject/img/malin/m4.jpg",
      img5Src : "/NewWebProject/img/malin/m5.jpg",
+     lat: 55.3527795,
+     long: -7.33208,
     },
          {name: "Slieve League",
      wikipediaURL : 'http://en.wikipedia.org/wiki/Slieve_League',
-     img1Src : "/NewWebProject/img/slieve/s1.Jpg",
+     img1Src : "/NewWebProject/img/slieve/s1.JPG",
      img2Src : "/NewWebProject/img/slieve/s2.jpg",
      img3Src : "/NewWebProject/img/slieve/s3.jpeg",
      img4Src : "/NewWebProject/img/slieve/s4.jpg",
      img5Src : "/NewWebProject/img/slieve/s5.jpg",
+               lat: 54.6367862,
+     long: -8.6814817,
     },
 ];
     
@@ -181,13 +152,8 @@ angularApp.controller('mainController', ['$scope', '$compile',
       console.log(summaryInfo);
       $scope.selectedItemWiki = summaryInfo;
     
-    $scope.$apply(); 
-//      for (key in summaryInfo) {
-//        $('.summary .' + key).text(summaryInfo[key]);
-//      }
-//      $('.summary .thumbnail').attr('src', summaryInfo.image);
-//      var dataAsJson = JSON.stringify(summaryInfo, null, '    ')
-//      $('.summary .raw').val(dataAsJson);    
+      $scope.$apply(); 
+ 
     }
                
     $scope.updateSelectedItem = function(selectedItem)
@@ -200,6 +166,7 @@ angularApp.controller('mainController', ['$scope', '$compile',
              { 
                 //$scope.updateImages(attraction);
                 $scope.selectedAttraction = attraction;
+                setMapLocation(attraction.lat,attraction.long);
                 //console.log(attraction.wikipediaURL);
                 var info = WIKIPEDIA.getData(attraction.wikipediaURL,$scope.updateSelectedItemText,function(error) {
                 alert(error);
@@ -210,176 +177,21 @@ angularApp.controller('mainController', ['$scope', '$compile',
 
     $scope.$apply();    
     }
-        
- 
-// 
-//        
-//        
-//            $scope.activateTime = new Date();
-//    $scope.loginTime = new Date();
-//    $scope.doneActivate = false;
-//    $scope.doneLogin = false;
-//    $scope.activateHandle = "";
-//        
-//    $scope.user = UserService.user;
-//    $scope.$broadcast( 'mainController.load' );
-//
-//        
-//    ua.onNotification = function()
-//    {
-//        console.log("Entered ua.onNotification(jsonObject) with notificationType == " + jsonObject.notificationType);
-//                var notificationType = jsonObject.notificationType;
-//    
-//                if (notificationType == 'User') {
-//                    $scope.handleUserNotification(jsonObject);
-//                } else if (notificationType == 'Work') {
-//                    $scope.handleWorkNotification(jsonObject);
-//                }
-//    }
-//        
-//        
-//        //HANDLE WORK NOTIFICATION
-//        $scope.handleWorkNotification = function(jsonObject) {
-//
-//            console.log("Entered handleWORKNotification(jsonObject)");
-//            console.log(jsonObject);
-//            var workitemid;
-//            var callingLineId;
-//            var workitemState;
-//
-//            if (jsonObject.newValue != null) {
-//
-//                if (jsonObject.newValue.workId != null) {
-//                    workitemid = jsonObject.newValue.workId;
-//                }
-//
-//                if (jsonObject.newValue.state != null) {
-//                    workitemState = jsonObject.newValue.state;
-//                }
-//
-//                if (jsonObject.newValue.callingLineId != null) {
-//                    callingLineId = jsonObject.newValue.callingLineId;
-//                }
-//
-//                if (workitemid != null) {
-//                    //currentWorkItemID = workitemid;
-//                }
-//
-//            }
-//
-//            if (jsonObject.changeType == 'New') {
-//
-//                if ($('#' + workitemid).length == 0) //if the workitem doesn't already exist
-//                {
-//                    //$scope.createWorkItem("voice", workitemid, workitemState, callingLineId);
-//                    UserService.addWork("voice", workitemid, workitemState, callingLineId);
-//                }
-//                
-//            } else if (jsonObject.changeType == 'Update') {
-//                
-//                if ($('#' + workitemid).length > 0) //if the workitem does exist
-//                {
-//                    //$scope.updateWorkItem(workitemid, workitemState);
-//                    UserService.updateWork(workitemid, workitemState);
-//                }
-//                
-//            } else if (jsonObject.changeType == 'Deleted') {
-//                 var currentWorkItemID =  $scope.currentSelectedWorkItemID;
-//                 //$scope.releaseWorkItem(currentWorkItemID);
-//                UserService.deleteWork(currentWorkItemID);
-//            }
-//
-//        };
-//
-//        //HANDLE USER NOTIFICATION
-//        $scope.handleUserNotification = function(jsonObject) {
-//            console.log("Entered handleUserNotification(jsonObject)");
-//            console.log(jsonObject);
-//
-//            UserService.setUserName(jsonObject.newValue.firstName, jsonObject.newValue.surname);
-//            $scope.$apply();
-//            
-//            if (jsonObject.newValue.userState == null) {
-//                
-//                UserService.setUserState("loggedout");
-//                
-//                $scope.$broadcast( 'handleUserNotification.activate' );
-//
-//                
-//            } else {
-//                
-//                var userState = jsonObject.newValue.userState;
-//                UserService.setUserState(userState);
-//                $scope.$apply();
-//                //If we are logged in
-//                if (jsonObject.newValue.userState.state === "LoggedIn") {
-//                    
-//                    $scope.$broadcast( 'handleUserNotification.login' );
-//                    
-//                } else {
-//                    //If we are logged out
-//                    if (jsonObject.newValue.userState.state === "LoggedOut") {
-//                        
-//                        $scope.$broadcast( 'handleUserNotification.logout' );
-//
-//                    }
-//
-//                }
-//            }
-//        };
-//        
-//
-//              $scope.doneActivate = false;
-//    $scope.doneLogin = false;
-//    
-//    $scope.compileUITemplates = function(jsonObject) {
-//
-//         $scope.compiledWITemplate = $compile('<udworkitem id="newWorkItem" ng-click="workItemClickEvent($event)"></udworkitem>')($scope);
-//         $scope.compiledLoginTemplate = $compile('<udloginform></udloginform>')($scope);
-//         $scope.compiledProfileTemplate = $compile('<udagentprofile></udagentprofile>')($scope);
-//         $scope.compiledAgentStateTemplate = $compile('<udagentstate></udagentstate>')($scope);
-//         $scope.compiledPerformanceTemplate = $compile('<udperformance></udperformance>')($scope);
-//         $scope.compiledCallDetailsTemplate = $compile('<udvoicecalldetails></udvoicecalldetails>')($scope);
-//    }
-//
-//    $scope.compileUITemplates();
-//  
-//        $scope.views = {
-//            loginView: 'login',
-//            activateView: 'activate',
-//            mainView: 'main',
-//            workitemView: 'workitems',
-//        };
-//
-//        $scope.setCurrentView = function(newView) {
-//            $scope.currentView = newView;
-//            
-//            console.log("CURRENT VIEW IS === " + $scope.currentView);
-//        }
-//                
-//        $scope.setCurrentView($scope.views.loginView);
-//        $scope.currentSelectedWorkItemID = 0;
-//        
-//
-//
-//
-//              //DOCUMENT READY EVENT
-//        angular.element(document).ready(function() {
-//        initMainApp();
-//
-//        });
-//        
-//        $scope.createTestWorkNotification = function() {
-//            var TestWorkNotification = new TestNotification("Work", "Work", "123456", "abc123", "New", "WorkState", true);
-//            $scope.handleNotification(TestWorkNotification);
-//        };
-//
-//
-//        
-//    
-//                                         
-//                                         
-//                                                                                  
+    
+    var init = function () {
+      console.log("Document loaded fired");
+           var elementHTML =  "Grianan of Aileach";
+          
+           $scope.updateSelectedItem(elementHTML);
+           console.log("Selected Item is : " + $scope.selectedItem);
+          
+    };
+    init();
+  
+    $scope.$on('$viewContentLoaded', function() {
+            
+    });
+                                                                                       
     }
                                          
 ]);
